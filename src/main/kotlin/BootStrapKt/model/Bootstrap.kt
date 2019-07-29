b3ad682a6379420a92bb336ddc722f2843a6120d
murakamitaichi <9khc727ru7@gmail.com>
2019-07-30 02:14:16 +0900

    平均

--- src/main/kotlin/BootStrapKt/model/Bootstrap.kt
+++ src/main/kotlin/BootStrapKt/model/Bootstrap.kt
@@ -1,3 +1,21 @@
 package BootStrapKt.model
 
+import java.util.*
+import kotlin.random.Random
+
 fun exampleAdd(num: Int) = num + 1
+
+fun bootstrapMean(sample: List<Double>, size: Int): List<Double> {
+    val result = LinkedList<Double>()
+    repeat(size) {
+        val resample = LinkedList<Double>()
+        repeat(sample.size) {
+            resample.add(sample[Random.nextInt(sample.size)])
+        }
+
+        val resampleMean = resample.sum()/resample.size
+        result.add(resampleMean)
+    }
+
+    return result.toList()
+}
--- src/test/kotlin/BootStrapKt/model/BootstrapKtTest.kt
+++ src/test/kotlin/BootStrapKt/model/BootstrapKtTest.kt
@@ -11,4 +11,14 @@ internal class BootstrapKtTest {
         assertEquals(2, exampleAdd(1))
         assertEquals(3, exampleAdd(2))
     }
+
+    @Test
+    fun bootstrapMean() {
+        val sample = listOf(
+                10.0, 20.0, 12.0, 23.5, 14.9, 32223.0, 32.0, 22.2, 1.2, 23.0
+        )
+        val act = BootStrapKt.model.bootstrapMean(sample, 1000)
+        assertEquals(1000, act.size)
+        assertEquals(3238.18, act.sum() / act.size, 300.0) // 10%くらいの誤差ならOK
+    }
 }
